8e9109f83447065048ea8fb9c2f50a12f85eb932
Phil-vP <68397038+Phil-vP@users.noreply.github.com>
2020-10-21 18:10:38 +0200

    Writer works so far, formatting after reading still strange

--- xml_ReadWrite/gml_writer.py
+++ xml_ReadWrite/gml_writer.py
@@ -9,62 +9,143 @@ import xml.etree.ElementTree as ET
 import xml.dom.minidom as MD
 
 
-global tree_root
+tree_root = {}
+POINT = 'gml:Point'
+POS = 'gml:Pos'
+POSLIST = 'gml:PosList'
+LINESTR = 'gml:LineString'
 
 
-def create_file():
+def create_root():
     global tree_root
-    # create the file structure
-    # tree_root = ET.Element('data')
-    # items = ET.SubElement(tree_root, 'itemsABC')
-    # item1 = ET.SubElement(items, 'item')
-    # item2 = ET.SubElement(items, 'item')
-    # item1.set('name','item1')
-    # item2.set('name','item2')
-    # item1.text = 'item1abc'
-    # item2.text = 'item2abc'
-    tree_root = ET.Element('Tracks')
-    flyingObject_1 = ET.SubElement(tree_root, "flyingObject")
-    # lastPos = ET.SubElement(flyingObject_1, "gml:Point")
-    # allPos = ET.SubElement(flyingObject_1, "gml:LineString")
-    
-    lastPos_Point = ET.SubElement(flyingObject_1, "gmlPoint")
-    lastPos_position = ET.SubElement(lastPos_Point, "gmlPos")
-    lastPos_position.set('srsDimension', "2")
-    lastPos_position.text = "asdf"
-    
-    allPos_LineString = ET.SubElement(flyingObject_1, "gmlLineString")
-    allPos_positions = ET.SubElement(allPos_LineString, "gmlPosList")
-    
-    
-    
-    write_file("items.xml")
+    # register Namespace gml
+    namespaces = {'xmlns:gml': 'http://www.opengis.net/gml'}
+
+    # create the tree root
+    tree_root = ET.Element('Tracks', namespaces)
 
 
-def addSubEl(new_name, parent):
+def update_position(track_id, lon, lat):
     global tree_root
+
+    selected_object = None
+
+    pos_str = str(lon) + " " + str(lat)
+
     for child in tree_root:
-        print(child.attrib)
-    
-    
+        att = child.attrib
+        if att['id'] == str(track_id):
+            selected_object = child
+            break
+
+    if selected_object is None:
+        print("Track with ID " + str(track_id) + " not found")
+        return
+
+    print("Track to be updated:")
+    print(selected_object.attrib)
+
+    print("Length: " + str(len(selected_object)))
+
+    # If the length is 0, it means that no position has been logged yet.
+    # We now have to initialize the first position
+    if len(selected_object) == 0:
+        lastPos_Point = ET.SubElement(selected_object, POINT)
+        lastPos_position = ET.SubElement(lastPos_Point, POS)
+        # lastPos_position.set('srsDimension', "2")
+        lastPos_position.text = ""
+
+        allPos_LineString = ET.SubElement(selected_object, LINESTR)
+        allPos_positions = ET.SubElement(allPos_LineString, POSLIST)
+        allPos_positions.text = ""
+        allPosText = pos_str
+
+    else:
+        lastPos_Point = selected_object[0]
+        lastPos_position = lastPos_Point[0]
+        allPos_LineString = selected_object[1]
+        allPos_positions = allPos_LineString[0]
+        allPosText = ", " + pos_str
+
+    # Now, all values are initialized.
+    # We can now update the position to the current one
+
+    lastPos_position.text = pos_str
+    allPos_positions.text += allPosText
+
+
+def update_classiden(track_id, classification_new, identification_new):
+    global tree_root
+
+    selected_object = None
+
+    for child in tree_root:
+        att = child.attrib
+        if att['id'] == str(track_id):
+            selected_object = child
+            break
+
+    if selected_object is None:
+        print("Track with ID " + str(track_id) + " not found")
+        return
+
+    att = selected_object.attrib
+
+    att['track_classification'] = str(classification_new)
+    att['track_identification'] = str(identification_new)
+
+    print("Track classIden updated")
+
+
+def update_full(track_id, lon, lat, class_new, iden_new):
+    update_position(track_id, lon, lat)
+    update_classiden(track_id, class_new, iden_new)
+
+
+def add_new_track(id_number, classification, identification):
+    global tree_root
+    flyingObject = ET.SubElement(tree_root, "flyingObject")
+
+    flyingObject.set("id", str(id_number))
+    flyingObject.set("track_classification", str(classification))
+    flyingObject.set("track_identification", str(identification))
+    return flyingObject
+
+
+def prettyPrint():
+    print(MD.parseString(ET.tostring(tree_root).decode()).toprettyxml())
+
+
+def addTracks(x):
+    # x = random.randint(2, 10)
+    for i in range(x):
+        add_new_track(i+1, "unknown", "unknown")
+
+    print("Added " + str(x) + " Tracks")
+
+
+def read_file(filename):
+    global tree_root
+    tree = ET.parse(filename)
+    tree_root = tree.getroot()
+
 
 def write_file(filename):
-    global tree_root
-    
     # create a new XML file with the results
+    print("\n    Writing File... \n")
     mydata = ET.tostring(tree_root).decode()
-    
+
     dom = MD.parseString(mydata)
+    # dom = expatbuilder.parseString(mydata, False)
     mydata_pretty = dom.toprettyxml()
-    print(mydata)
     print(mydata_pretty)
     with open(filename, "w+") as myfile:
         myfile.write(mydata_pretty)
-    
 
 
-
-if __name__== "__main__":
+if __name__ == "__main__":
     print("XML Writer")
-    create_file()
+    create_root()
+    addTracks(5)
+    prettyPrint()
     print("Done")
