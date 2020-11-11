416d845993f94f13ef8413231c062208491ee21f
Phil-vP <68397038+Phil-vP@users.noreply.github.com>
2020-11-11 12:18:47 +0100

    Added json readWrite

--- geojson_ReadWrite/geojson_writer.py
+++ geojson_ReadWrite/geojson_writer.py
@@ -8,21 +8,27 @@ Created on Tue Oct 20 12:09:34 2020
 import geojson
 
 lastKnownPositions = {}
+positionHistory = {}
 
 
 
 def update_position(track_id, lon, lat):
     global lastKnownPositions
+    global positionHistory
     
     id_str = str(track_id)
     
     # If the Track is already created, this will do nothing
     addNewTrack(track_id)
     
-    updatedPoint = geojson.Point((lon, lat))
+    updatedPoint = (lon, lat)
     
     feature = lastKnownPositions[id_str]
-    feature["geometry"] = updatedPoint
+    feature.geometry.coordinates = updatedPoint
+    
+    feature_ls = positionHistory[id_str]
+    feature_ls.geometry.coordinates.append(updatedPoint)
+    
     
     # print("Updated Track " + id_str + " to " + str(updatedPoint))
     # print(updatedPoint)
@@ -38,21 +44,41 @@ def update_classiden(track_id, classification_new, identification_new):
     addNewTrack(track_id)
     
     feature = lastKnownPositions[id_str]
-    updatedProperties = feature['properties']
+    fProperties = feature.properties
     
-    updatedProperties['classification'] = str(classification_new)
-    updatedProperties['identification'] = str(identification_new)
+    fProperties['classification'] = str(classification_new)
+    fProperties['identification'] = str(identification_new)
     
-    feature['properties'] = updatedProperties
+    
+    feature_ls = positionHistory[id_str]
+    fProperties_ls = feature_ls.properties
+    
+    fProperties_ls['classification'] = str(classification_new)
+    fProperties_ls['identification'] = str(identification_new)
 
 
 def update_full(track_id, lon, lat, class_new, iden_new):
     update_position(track_id, lon, lat)
-    # update_classiden(track_id, class_new, iden_new)
+    update_classiden(track_id, class_new, iden_new)
 
+
+def update_sidc(track_id, value):
+    global lastKnownPositions
+    
+    id_str = str(track_id)
+    
+    # If the Track is already created, this will do nothing
+    addNewTrack(track_id)
+    
+    feature = lastKnownPositions[id_str]
+    updatedProperties = feature['properties']
+    updatedProperties['sidc'] = str(value)
+    
+    feature['properties'] = updatedProperties
 
 def addNewTrack(track_id):
     global lastKnownPositions
+    global positionHistory
     
     id_str = str(track_id)
     
@@ -66,8 +92,16 @@ def addNewTrack(track_id):
     properties = {}
     properties['classification'] = "unknown"
     properties['identification'] = "unknown"
-    f = geojson.Feature(id_str, newPoint, properties)
-    lastKnownPositions[id_str] = f
+    f_p = geojson.Feature(id_str, newPoint, properties)
+    lastKnownPositions[id_str] = f_p
+    
+    zerotuple = (0,0)
+    coo_list = []
+    
+    newLineString = geojson.LineString(coo_list)
+    f_ls = geojson.Feature(id_str, newLineString, properties.copy())
+    positionHistory[id_str] = f_ls
+    
     
 
 
@@ -76,7 +110,8 @@ def prettyPrint():
     keys = list(lastKnownPositions.keys())
     for k in keys:
         print("ID: " + str(k))
-        print("Feature: " + str(lastKnownPositions[k]))
+        print("Feature lastKnownPositions: " + str(lastKnownPositions[k]))
+        print("Feature positionHistory: " + str(positionHistory[k]))
         print("================")
 
 
@@ -96,7 +131,9 @@ def write_file(filename):
     # create a new XML file with the results
     print("\n    Writing File... \n")
     
-    feature_list = list(lastKnownPositions.values())
+	feature_list = []
+    feature_list.extend(list(lastKnownPositions.values()))
+    feature_list.extend(list(positionHistory.values()))
     feature_collection = geojson.FeatureCollection(feature_list)
     
     with open(filename, "w+") as myfile:
@@ -105,10 +142,16 @@ def write_file(filename):
 
 def testRun():
     print("test run")
-    update_position(1, 10, 15)
-    update_position(2, 8, 7)
+    update_position(1, 11.49, 48.77)
+    update_position(1, 11.47, 48.80)
+    update_position(1, 11.44, 48.81)
+    update_position(2, 11.50, 48.81)
+    update_position(2, 11.48, 48.82)
     update_classiden(1, "friendly", "AWACS")
+    update_sidc(1, "SFGPUCI----F---")
+	update_sidc(2, "SUAP--------")
     write_file("test1.json")
+    prettyPrint()
 
 
 if __name__ == "__main__":
